51ad52abad1528242fd98214a59d58de7c6759b3
Constantine Sapuntzakis <csapuntz@gmail.com>
2026-03-22 13:42:42 -0700

    More ES6

--- js/siteblock.js
+++ js/siteblock.js
@@ -1,81 +1,59 @@
 // Copyright 2012 Constantine Sapuntzakis
 //
 
-if (typeof csapuntz == "undefined") {
-   var csapuntz = {};
-}
-
-csapuntz.siteblock = (function () {
-   var sbself = {
-      read_options: function (stg) {
-         var opts = {};
-
-         if ("settings" in stg) {
-            opts = JSON.parse(stg['settings']);
-         }
-
-         if (!("rules" in opts))
-            opts.rules = "";
-
-         if (!("allowed" in opts))
-            opts.allowed = 0;
-
-         if (!("period" in opts))
-            opts.period = 1440;
-
-         return opts;
+const siteblock = {
+      read_options(stg) {
+         const opts = "settings" in stg ? JSON.parse(stg['settings']) : {};
+         const { rules = "", allowed = 0, period = 1440 } = opts;
+         return { ...opts, rules, allowed, period };
       },
 
-      newUsageTracker: function () {
-         var time_cb = function () {
-            var d = new Date();
+      newUsageTracker() {
+         let time_cb = () => Date.now() / 1000;
 
-            return d.getTime() / 1000;
-         };
+         let time_allowed = 0;
+         let time_period = 3600;
 
-         var time_allowed = 0;
-         var time_period = 3600;
+         let time_used = 0;
 
-         var time_used = 0;
+         let last_start = -1;
+         let last_end = -1;
 
-         var last_start = -1;
-         var last_end = -1;
-
-         var check_reset = function (now) {
+         const check_reset = (now) => {
             if (last_end != -1 &&
                (now - last_end) >= (time_period - time_allowed)) {
                time_used = 0;
             }
-         }
+         };
 
-         var get_time_used = function () {
-            var time = time_cb();
+         const get_time_used = () => {
+            const time = time_cb();
 
             check_reset(time);
 
             // If we're in an existing interval, count it
-            var time_adj = 0;
+            let time_adj = 0;
             if (last_start != -1 && time > last_start && time < last_start + 120)
                time_adj = (time - last_start);
 
             return (time_used + time_adj);
          };
 
-         var self = {
-            onBlockedSiteAllowed: function () {
+         const self = {
+            onBlockedSiteAllowed() {
                if (last_start == -1) {
                   last_start = time_cb();
                }
             },
 
-            onLastBlockedDone: function () {
+            onLastBlockedDone() {
                self.updateTimeUsed();
                last_start = -1;
             },
 
-            updateTimeUsed: function () {
+            updateTimeUsed() {
                if (last_start != -1) {
-                  var end = time_cb();
+                  const end = time_cb();
                   if (end > last_start + 120) {
                      // Don't count the time
                   } else if (end > last_start) {
@@ -87,28 +65,24 @@ csapuntz.siteblock = (function () {
                }
             },
 
-            allowed: function () {
+            allowed() {
                return get_time_used() < time_allowed;
             },
 
-            setInterval: function (allowed, period) {
+            setInterval(allowed, period) {
                time_allowed = allowed * 60;
                time_period = period * 60;
             },
 
-            setTimeCallback: function (new_cb) {
+            setTimeCallback(new_cb) {
                time_cb = new_cb;
             },
 
-            getState: function () {
-               return {
-                  "time_used": time_used,
-                  "last_start": last_start,
-                  "last_end": last_end,
-               };
+            getState() {
+               return { time_used, last_start, last_end };
             },
 
-            setState: function (st) {
+            setState(st) {
                time_used = st.time_used;
                last_start = ("last_start" in st) ? st.last_start : -1;
                last_end = st.last_end;
@@ -118,52 +92,48 @@ csapuntz.siteblock = (function () {
          return self;
       },
 
-      newSiteBlock: function () {
-         var path_white;
-         var path_black;
+      newSiteBlock() {
+         let path_white = [];
+         let path_black = [];
 
-         var tracked_tabs = [];
+         let tracked_tabs = [];
 
-         var ut = sbself.newUsageTracker();
+         const ut = siteblock.newUsageTracker();
 
-         var self = {
-            updatePaths: function (paths) {
+         const self = {
+            updatePaths(paths) {
                if (paths === undefined)
                   return;
 
-               paths = paths.split("\n");
-               path_white = new Array();
-               path_black = new Array();
+               path_white = [];
+               path_black = [];
 
-               for (var i = 0; i < paths.length; ++i) {
-                  var p = paths[i];
-                  if (p.match(/^\s*$/)) {
-                  } else {
-                     var add = path_black;
-                     if (p[0] == '+') {
-                        p = p.substr(1);
-                        add = path_white;
-                     }
-                     p = p.replace(/\./g, '\\.');
-                     p = p.replace(/\*/g, '.*');
-                     add.push(new RegExp(p, 'ig'));
+               for (let p of paths.split("\n")) {
+                  if (p.match(/^\s*$/))
+                     continue;
+
+                  let add = path_black;
+                  if (p[0] === '+') {
+                     p = p.slice(1);
+                     add = path_white;
                   }
+                  p = p.replace(/\./g, '\\.').replace(/\*/g, '.*');
+                  add.push(new RegExp(p, 'ig'));
                }
             },
 
-            isBlocked: function (url) {
-               var blocked = false;
+            isBlocked(url) {
+               let blocked = false;
 
                if (url !== undefined && url.match(/https?:/)) {
-                  var p;
-                  for (p in path_black) {
-                     if (url.search(path_black[p]) != -1) {
+                  for (const pattern of path_black) {
+                     if (url.search(pattern) != -1) {
                         blocked = true;
                         break;
                      }
                   }
-                  for (p in path_white) {
-                     if (url.search(path_white[p]) != -1) {
+                  for (const pattern of path_white) {
+                     if (url.search(pattern) != -1) {
                         blocked = false;
                         break;
                      }
@@ -177,14 +147,14 @@ csapuntz.siteblock = (function () {
 
             setTimeCallback: ut.setTimeCallback,
 
-            getState: function () {
+            getState() {
                return {
-                  "ut": ut.getState(),
-                  "tabs": tracked_tabs.slice(),
+                  ut: ut.getState(),
+                  tabs: tracked_tabs.slice(),
                };
             },
 
-            setState: function (st) {
+            setState(st) {
                if ("ut" in st) {
                   ut.setState(st.ut);
                }
@@ -193,14 +163,14 @@ csapuntz.siteblock = (function () {
                }
             },
 
-            startTracking: function (tabid) {
+            startTracking(tabid) {
                if (!tracked_tabs.includes(tabid)) {
                   tracked_tabs.push(tabid);
                }
             },
 
-            stopTracking: function (tabid) {
-               var idx = tracked_tabs.indexOf(tabid);
+            stopTracking(tabid) {
+               const idx = tracked_tabs.indexOf(tabid);
                if (idx != -1) {
                   tracked_tabs.splice(idx, 1);
                   return true;
@@ -208,13 +178,13 @@ csapuntz.siteblock = (function () {
                return false;
             },
 
-            emptyTracking: function () {
+            emptyTracking() {
                return tracked_tabs.length == 0;
             },
 
-            blockThisTabChange: function (tabid, url) {
-               var blocked = (url !== null) ? self.isBlocked(url) : false;
-               var allowed = ut.allowed();
+            blockThisTabChange(tabid, url) {
+               const blocked = (url !== null) ? self.isBlocked(url) : false;
+               const allowed = ut.allowed();
 
                if (blocked) {
                   self.startTracking(tabid);
@@ -229,7 +199,7 @@ csapuntz.siteblock = (function () {
                return blocked && !allowed;
             },
 
-            getBlockedTabs: function () {
+            getBlockedTabs() {
                if (ut.allowed()) {
                   return [];
                }
@@ -237,16 +207,13 @@ csapuntz.siteblock = (function () {
                return tracked_tabs.slice();
             },
 
-            updateTimeUsed: function () {
+            updateTimeUsed() {
                ut.updateTimeUsed();
             },
          }; // self =
 
          return self;
       } // newSiteBlock
-   }; // self =
+};
 
-   return sbself;
-})();
-
-export default csapuntz;
+export default { siteblock };
